d29af6ca04784b590284d1e0857d7fa524f18e98
Hadrian Rafael <hadrianrafael82@gmail.com>
2026-08-06 19:48:10 -0300

    chore(config): add shared tsconfig and eslint flat configs

---
+++ packages/config/eslint/nextjs.mjs
@@ -0,0 +1,6 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import base from './base.mjs';
+
+const compat = new FlatCompat({ baseDirectory: import.meta.dirname });
+
+export default [...base, ...compat.extends('next/core-web-vitals')];
